929d40dfdaf2b188a5b96f9e80c1b0fc4ac01326
PPIP <agnesspei731@gmail.com>
2026-03-30 17:23:21 -0500

    analytics

--- patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -1,14 +1,36 @@
 package com.pm.patientservice.kafka;
 
+import com.pm.patientservice.grpc.BillingServiceGrpcClient;
+import com.pm.patientservice.model.Patient;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+import org.slf4j.Logger;
 
 @Service
 public class KafkaProducer {
+    private static final Logger log = LoggerFactory.getLogger(BillingServiceGrpcClient.class);
     private final KafkaTemplate<String, byte[]> kafkaTemplate;
     public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
 
+    }
+
+    public void sendMessage(Patient patient) {
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT_CREATED")
+                .build();
+
+        try{
+            kafkaTemplate.send("patient", event.toByteArray());
+
+        } catch (Exception e){
+            log.error("Error sending PatientCreated event: {}", event);
+        }
     }
 
 
